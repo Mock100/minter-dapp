987a7e2844c55e35ffd59f41f7ed168242edb4ec
Mock100 <leemockler@yahoo.com>
2022-04-06 13:22:14 +0200

    Rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xcd3298f65BA263E12873d3f2d654D1569B39af90"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
