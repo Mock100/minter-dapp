41f12ffc95755245dacb1f468f9f86f45febba61
Mock100 <leemockler@yahoo.com>
2022-04-06 16:16:08 +0200

    updated fonts

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0xcd3298f65BA263E12873d3f2d654D1569B39af90"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Test Emoji NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'When you buy a Bored Golfer, you are not only buying an avatar or provable rare piece of art, you are gaining membership access to a club whose benefits and offerings will increase over time, your Bored Golfer can serve as your digital identity and open doors for you';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
